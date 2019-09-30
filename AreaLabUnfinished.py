61778298ba4e1d975c2dbf1a515c274c6afd1669
JoyP321 <joy@djp3.net>
2019-09-30 09:58:38 -0700

    add area formulas and test cases

--- AreaLabUnfinished.py
+++ AreaLabUnfinished.py
@@ -1,26 +1,31 @@
-import math 
+import math #math and unittest library has pre-written math functions
 import unittest
 
-def circleArea(radius): 
-    return 1
+def circleArea(radius):
+    """Returns the area of a circle"""
+    return math.pi*radius*radius
 
 def rectArea(base, height):
-    return 1
+    """Returns the area of a rectangle"""
+    return base*height
 
 def trapArea(base1, base2, height):
-    return 1
+    """Returns the area of a trapezoid"""
+    return (base1+base2)/2*height
 
 def triArea(base,height):
-    return 1
+    """Returns the area of a triangle"""
+    return 0.5*base*height
     
 	
-class MyTest(unittest.TestCase):
+class MyTest(unittest.TestCase): 
     def testCircleArea(self):
-    
+        self.assertAlmostEqual(circleArea(5), math.pi*25)
     def testRectArea(self):
-    
+        self.assertAlmostEqual(rectArea(2,3), 6)
     def testTrapArea(self):
-    
+        self.assertAlmostEqual(trapArea(3,6,2), 9)
     def testTriArea(self):
+        self.assertAlmostEqual(triArea(4,7), 14)
     
     
